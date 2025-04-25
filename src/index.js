a08bb147c8c9365438211b907b202ec643842ab4
AirSurfer09 <saurav@convai.com>
2025-04-26 02:22:25 +0530

    code utilities

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-console.log(process.env.PORT);
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening on port: ${process.env.PORT || 8000}`);
+    })
+  )
+  .catch((err) => {
+    console.log("MONGODB connection error:", err);
+  });
